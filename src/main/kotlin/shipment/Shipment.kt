cb07bcf52a5e322d93ee637e0329e4cd14145866
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-07-13 14:48:27 -0600

    started with shipment class and simulation

--- src/main/kotlin/shipment/Shipment.kt
+++ src/main/kotlin/shipment/Shipment.kt
@@ -1,4 +1,19 @@
 package shipment
 
-class Shipment {
+class Shipment (
+    status: String,
+    id: String,
+    notes: ArrayList<String>,
+    updateHistory: ArrayList<ShippingUpdate>,
+    expectedDeliveryDateTimeStamp: Long,
+    currentLocation: String,
+    private var observers: ArrayList<ShipmentObserver>
+){
+    var notes: ArrayList<String> = notes
+        private set
+
+    var updateHistory: ArrayList<ShippingUpdate> = updateHistory
+        private set
+
+
 }
--- src/main/kotlin/shipment/TrackingSimulator.kt
+++ src/main/kotlin/shipment/TrackingSimulator.kt
@@ -1,4 +1,35 @@
 package shipment
 
-class TrackingSimulator {
+import kotlinx.coroutines.*
+
+import shipment.updateStrategy.CanceledUpdateStrategy
+import shipment.updateStrategy.DelayedUpdateStrategy
+import shipment.updateStrategy.DeliveredUpdateStrategy
+import shipment.updateStrategy.LocationUpdateStrategy
+import shipment.updateStrategy.LostUpdateStrategy
+import shipment.updateStrategy.NoteAddedUpdateStrategy
+import shipment.updateStrategy.ShipmentUpdateStrategy
+import shipment.updateStrategy.ShippedUpdateStrategy
+
+class TrackingSimulator(
+    val shipments: MutableMap<String, Shipment> = mutableMapOf(),
+    val strategyMap: Map<String, ShipmentUpdateStrategy> = mapOf<String, ShipmentUpdateStrategy>(
+        "shipped" to ShippedUpdateStrategy(),
+        "location" to LocationUpdateStrategy(),
+        "delayed" to DelayedUpdateStrategy(),
+        "noteadded" to NoteAddedUpdateStrategy(),
+        "lost" to LostUpdateStrategy(),
+        "canceled" to CanceledUpdateStrategy(),
+        "delivered" to DeliveredUpdateStrategy()
+    )
+) {
+    fun addShipment(shipment: Shipment) {
+        shipments.put(shipment.id, shipment)
+    }
+
+    fun findShipment(id: String) = shipments[id]
+
+    fun runSimulation() {
+
+    }
 }
