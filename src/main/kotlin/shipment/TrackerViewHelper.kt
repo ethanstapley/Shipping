f8c437017d4cc4115904d90634e3038ec6a7f5fe
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-07-13 17:57:35 -0600

    first steps to the tracker view helper

--- src/main/kotlin/shipment/TrackerViewHelper.kt
+++ src/main/kotlin/shipment/TrackerViewHelper.kt
@@ -1,4 +1,25 @@
 package shipment
 
-class TrackerViewHelper {
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class TrackerViewHelper: shipment.ShipmentObserver{
+    private val _shipmentId = MutableStateFlow("")
+    val shipmentId: StateFlow<String> = _shipmentId
+
+    private val _shipmentNotes = MutableStateFlow<ArrayList<String>>(arrayListOf())
+    val shipmentNotes: StateFlow<ArrayList<String>> = _shipmentNotes
+
+    private val _shipmentUpdateHistory = MutableStateFlow<ArrayList<String>>(arrayListOf())
+    val shipmentUpdateHistory: StateFlow<ArrayList<String>> = _shipmentUpdateHistory
+
+    private val _expectedShipmentDeliveryDate = MutableStateFlow("")
+    val expectedShipmentDeliveryDate: StateFlow<String> = _expectedShipmentDeliveryDate
+
+    private val _shipmentStatus = MutableStateFlow("")
+    val shipmentStatus: StateFlow<String> = _shipmentStatus
+
+    override fun onShipmentUpdated(shipment: Shipment) {
+
+    }
 }
